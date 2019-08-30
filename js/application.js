650d2915115b1b5c05e54f391e29dd5cc230fb9a
2048cupcake <52897596+2048cupcake@users.noreply.github.com>
2019-08-30 16:57:08 -0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -8,10 +8,6 @@ function handle_undo() {
 }
 
 function setpack() {
-url = document.getElementById("pack").value
-  var lastChar = url.substr(-1); // Selects the last character
-if (lastChar != '/') {         // If the last character is not a slash
-   url = url + '/';  }          // Append a slash to it.
-localStorage.setItem("pack", url); 
+localStorage.setItem("pack", document.getElementById("pack").value); 
 location.reload();
 }
